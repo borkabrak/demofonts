7cb013b5d5010175d8787f8d3aea1dc77d31dc4b
Jonathan Carter <borkabrak@gmail.com>
2015-02-22 00:52:59 -0600

    The reset button clears samples.
    * Also factor 'document.querySelector()' into a shorter function name

--- tryfont.js
+++ tryfont.js
@@ -1,5 +1,10 @@
 var sample_text = "And that, I think, was the handle—that sense of inevitable victory over the forces of Old and Evil. Not in any mean or military sense; we didn't need that. Our energy would simply prevail. There was no point in fighting—on our side or theirs. We had all the momentum; we were riding the crest of a high and beautiful wave.…";
 
+var select = function(selector){
+    // return a single element determined by the given selector
+    return document.querySelector(selector);
+};
+
 function createSample(name){
     // create a sample div with the given font face
     var sample = document.createElement("div");
@@ -32,7 +37,7 @@ var tid = setInterval( function () {
     if ( document.readyState !== 'complete' ) return;
     clearInterval( tid );
 
-    document.querySelector("form#addfont").onsubmit = function (){
+    select("form#addfont").onsubmit = function (){
         var name = this.newfont.value;
 
         document.head.appendChild(createFontLink(name));
@@ -40,6 +45,10 @@ var tid = setInterval( function () {
         return false;
     }
 
-    document.querySelector("input").focus();
+    select("[type=reset]").onclick = function(event){
+        select("#samples").innerHTML = "";
+    };
+
+    select("input").focus();
 
 }, 100 );
